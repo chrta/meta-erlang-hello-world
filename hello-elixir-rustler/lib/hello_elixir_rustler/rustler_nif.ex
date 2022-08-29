b36cefbdbbf456a1256f19f0c89e85091ebd1e4c
Christian Taedcke <hacking@taedcke.com>
2022-08-29 18:17:51 +0200

    Config rustler compile target

--- hello-elixir-rustler/lib/hello_elixir_rustler/rustler_nif.ex
+++ hello-elixir-rustler/lib/hello_elixir_rustler/rustler_nif.ex
@@ -1,7 +1,8 @@
 defmodule HelloElixirRustler.RustlerNif do
   use Rustler,
     otp_app: :hello_elixir_rustler,
-    crate: "helloelixirrustler_rustlernif"
+    crate: "helloelixirrustler_rustlernif",
+    target: System.get_env("HOST_SYS")
 
   # When your NIF is loaded, it will override this function.
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
